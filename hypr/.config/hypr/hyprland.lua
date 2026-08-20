d21981f88a10953c73bc482ddbf26359e1e5c420
fabiobrizotti <fabioluisbrizotti@gmail.com>
2026-08-20 16:10:52 -0300

    feat(theming): icones papirus orange, cursor bibata e configs gtk/qt

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -5,23 +5,29 @@
 ------------------
 ---- DARK MODE ---
 ------------------
-hl.on("hyprland.start", function()
-    hl.exec_cmd('gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"')
-    hl.exec_cmd('gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"')
-
-    -- Autostart dos processos essenciais de forma limpa
-    hl.exec_cmd("waybar")
-    hl.exec_cmd("hyprpaper")
-    hl.exec_cmd("swayosd-server")
-    hl.exec_cmd("swaync")
-    hl.exec_cmd("hypridle")
+hl.on("hyprland.start", function () 
+  -- Temas GTK e Preferência Escura
+  hl.exec_cmd('gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"')
+  hl.exec_cmd('gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"')
+  hl.exec_cmd('gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"')
+  hl.exec_cmd('gsettings set org.gnome.desktop.interface cursor-theme "Bibata-Modern-Classic"')
+  hl.exec_cmd('gsettings set org.gnome.desktop.interface cursor-size 24')
+  
+  -- Autostart dos processos essenciais
+  hl.exec_cmd("waybar")
+  hl.exec_cmd("hyprpaper")
+  hl.exec_cmd("swayosd-server")
+  hl.exec_cmd("swaync")
+  hl.exec_cmd("hypridle")
 end)
 
+-- Variáveis de Ambiente para GTK, Qt e Cursores
 hl.env("GTK_THEME", "adw-gtk3-dark")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
 hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
 hl.env("HYPRCURSOR_SIZE", "24")
-
 ------------------
 ---- MONITORS ----
 ------------------
